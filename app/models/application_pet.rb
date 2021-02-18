f0af7dd69b3cbec9d56d4bed5abde9d07521d5fa
Alex Schwartz <aschwartz@detaso.com>
2021-02-17 21:46:19 -0600

    Wip, update application pets to handle statuses

--- app/models/application_pet.rb
+++ app/models/application_pet.rb
@@ -1,4 +1,6 @@
 class ApplicationPet < ApplicationRecord
   belongs_to :application
   belongs_to :pet
+
+  enum status: { pending: 1, accepted: 2, rejected: 3 }
 end
--- spec/models/application_pet_spec.rb
+++ spec/models/application_pet_spec.rb
@@ -5,4 +5,32 @@ describe ApplicationPet, type: :model do
     it { should belong_to :application }
     it { should belong_to :pet }
   end
+
+  describe 'validations' do
+    it { should define_enum_for(:status).with(pending: 1, accepted: 2, rejected: 3) }
+  end
+
+  # describe 'instance methods' do
+  #   describe '#pretty_status' do
+  #     it 'prettify :in_progress' do
+  #       application = Application.create!(application_params(:in_progress))
+  #       expect(application.pretty_status).to eq('In Progress')
+  #     end
+
+  #     it 'prettify :pending' do
+  #       application = Application.create!(application_params(:pending))
+  #       expect(application.pretty_status).to eq('Pending')
+  #     end
+
+  #     it 'prettify :accepted' do
+  #       application = Application.create!(application_params(:accepted))
+  #       expect(application.pretty_status).to eq('Accepted')
+  #     end
+
+  #     it 'prettify :rejected' do
+  #       application = Application.create!(application_params(:rejected))
+  #       expect(application.pretty_status).to eq('Rejected')
+  #     end
+  #   end
+  # end
 end
